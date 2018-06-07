06c70389664ed9de99493b8ca067e0bb4da7d9a7
Scott Haskell <shaskell@splunk.com>
2018-06-07 16:16:19 -0700

    multi-heatmap support and documentation

--- appserver/static/visualizations/maps-plus/src/maps-plus.js
+++ appserver/static/visualizations/maps-plus/src/maps-plus.js
@@ -190,6 +190,7 @@ define([
                                'clusterGroup',
                                'pathColor',
                                'popupAnchor',
+                               'heatLayer',
                                'heatPointIntensity',
                                '_time'];
             $.each(obj, function(key, value) {
@@ -711,8 +712,10 @@ define([
                 }
                 
 
-                if (this.isArgTrue(heatmapEnable) && !_.isUndefined(this.heat)) {
-                    this.heat.addTo(this.map);
+                if (this.isArgTrue(heatmapEnable) && !_.isEmpty(this.heatLayers)) {
+                    _.each(this.heatLayers, function(heat) {
+                        heat.addTo(this.map);    
+                    }, this)
                 }
 
                 if(this.isArgTrue(autoFitAndZoom)) {
@@ -990,14 +993,15 @@ define([
                 
                 var pathLineLayer = this.pathLineLayer = L.layerGroup();
                 
-                // Heatmap
-                if (this.isArgTrue(heatmapEnable)) {
-                    var heat = this.heat = L.heatLayer([], {minOpacity: heatmapMinOpacity,
-                                                            maxZoom: heatmapMaxZoom,
-                                                            radius: heatmapRadius,
-                                                            blur: heatmapBlur});
-                                                            //blur: heatmapBlur}).addTo(this.map);
-                }
+                var heatLayers = this.heatLayers = {};
+                // // Heatmap
+                // if (this.isArgTrue(heatmapEnable)) {
+                //     var heat = this.heat = L.heatLayer([], {minOpacity: heatmapMinOpacity,
+                //                                             maxZoom: heatmapMaxZoom,
+                //                                             radius: heatmapRadius,
+                //                                             blur: heatmapBlur});
+                //                                             //blur: heatmapBlur}).addTo(this.map);
+                // }
                
                 // Init defaults
                 if(this.isSplunkSeven) {
@@ -1066,10 +1070,21 @@ define([
 
                 // Add heatmap layer
                 if (this.isArgTrue(heatmapEnable)) {
+                    var heatLayer = this.heatLayer = _.has(userData, "heatLayer") ? userData["heatLayer"]:"default";
+
+                    if(!_.has(this.heatLayers, this.heatLayer)) {
+                        // Create heat layer
+                        this.heatLayers[this.heatLayer] = L.heatLayer([], {minOpacity: heatmapMinOpacity,
+                                                                           maxZoom: heatmapMaxZoom,
+                                                                           radius: heatmapRadius,
+                                                                           blur: heatmapBlur});
+                                                                    //blur: heatmapBlur}).addTo(this.map);
+                    }
+
                     var pointIntensity = this.pointIntensity = _.has(userData, "heatPointIntensity") ? userData["heatPointIntensity"]:1.0;
                     var heatLatLng = this.heatLatLng = L.latLng(parseFloat(userData['latitude']), parseFloat(userData['longitude']), parseFloat(this.pointIntensity));
                     console.log(heatLatLng);
-                    this.heat.addLatLng(this.heatLatLng);
+                    this.heatLayers[this.heatLayer].addLatLng(this.heatLatLng);
                     this.heatMarkers += 1;
                     
                     if(this.isArgTrue(heatmapOnly)) {

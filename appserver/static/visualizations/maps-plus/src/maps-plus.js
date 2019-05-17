649ccd4f6d03f97f1c791df92ae75ba9b941c63d
Scott Haskell <shaskell614@yahoo.com>
2019-05-17 10:38:13 -0700

    added check back to _addUnclustered

--- appserver/static/visualizations/maps-plus/src/maps-plus.js
+++ appserver/static/visualizations/maps-plus/src/maps-plus.js
@@ -1209,14 +1209,14 @@ define([
         _addUnclustered: function(map, options) {
             _.chain(options.layerFilter)
             .sortBy(function(d) {
-                if(!_.isUndefined(d.circle.layerPriority)){
+                if(_.has(d.circle, "layerPriority") && !_.isUndefined(d.circle.layerPriority)){
                     return +d.circle.layerPriority
                 } else {
                     return d
                 }                
             })
             .each(function(lg) {
-                if(!_.isUndefined(lg.circle.layerPriority)){
+                if(_.has(lg.circle, "layerPriority") && !_.isUndefined(lg.circle.layerPriority)){
                     map.createPane(options.paneZIndex.toString())
                     map.getPane(options.paneZIndex.toString()).style.zIndex = options.paneZIndexs
                 }
@@ -1230,7 +1230,7 @@ define([
                     }
                 })
 
-                if(!_.isUndefined(lg.circle.layerPriority)){
+                if(_.has(lg.circle, "layerPriority") && !_.isUndefined(lg.circle.layerPriority)){
                     lg.group.setStyle({pane: options.paneZIndex.toString()})
                     options.paneZIndex += 1
                 }
